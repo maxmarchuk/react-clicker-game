6ba23e9ae64a09ab462cb9fbb0de5111552d3ff5
Max Marchuk <mmarchuk@jamasoftware.com>
2017-09-02 14:32:06 -0700

    Wire up reset button to reset the game's state

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -1,19 +1,21 @@
-import React, {
-    Component,
-    PropTypes,
-} from 'react';
+import React, {Component } from 'react';
+import PropTypes from 'prop-types';
 
 import {Button} from "react-bootstrap";
 
 class ResetButton extends Component {
     render() {
         return (
-            <Button bsStyle="danger">Reset</Button>
+            <Button bsStyle="danger" onClick={this.props.onResetClick}>
+                Reset
+            </Button>
         );
     }
 }
 
-ResetButton.propTypes = {};
+ResetButton.propTypes = {
+    onResetClick: PropTypes.bool
+};
 ResetButton.defaultProps = {};
 
 export default ResetButton;
