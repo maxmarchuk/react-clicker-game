d85f8f8f22dad21ead58cc84c2fa5cad90f4d965
Max Marchuk <maxmarchuk@gmail.com>
2018-12-15 15:03:27 -0800

    update to latest react, react-dom, and use prop-types module

--- src/components/MinionButton.js
+++ src/components/MinionButton.js
@@ -1,31 +1,31 @@
-import React, {
-    Component,
-    PropTypes,
-} from 'react';
-
-import {Button} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { number, bool } from 'prop-types';
+import { Button } from 'react-bootstrap';
 
 class MinionButton extends Component {
-    render() {
-        const {amount, price, disabled} = this.props;
-        return (
-            <Button disabled={disabled} onClick={() => this.props.onClick(amount, price)}>
-                Buy Minion ({price})
-            </Button>
-        );
-    }
+  render() {
+    const { amount, price, disabled } = this.props;
+    return (
+      <Button
+        disabled={disabled}
+        onClick={() => this.props.onClick(amount, price)}
+      >
+        Buy Minion ({price})
+      </Button>
+    );
+  }
 }
 
 MinionButton.propTypes = {
-    amount: PropTypes.number,
-    price: PropTypes.number,
-    disabled: PropTypes.bool
+  amount: number,
+  price: number,
+  disabled: bool
 };
 
 MinionButton.defaultProps = {
-    amount: 0,
-    price: 0,
-    disabled: true
+  amount: 0,
+  price: 0,
+  disabled: true
 };
 
 export default MinionButton;
